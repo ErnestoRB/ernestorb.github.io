bfb17c08561746bf020881ee61c99da216f06355
Ernesto Ramírez <ernestorb_@outlook.com>
2023-03-13 00:01:22 -0600

    Añadir ResizeObserver pollyfil & añadir soporte para navegadores antiguos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+  plugins: [react(), legacy({ targets: ">0.3%, defaults, iOS >=12.0" })],
 });
